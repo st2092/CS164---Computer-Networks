d5a81184ccca727c77fc84618e042946a0374455
st2092 <sto002@ucr.edu>
2015-05-01 16:28:25 -0700

    Update lab5_rdt3.0_sender_client.py
    integrated checksum for ACKs, but that would need implementation of a timer for re-transmission of ACKs. For purpose of the lab, we assume all ACKs arrive to receiver uncorrupted and that only data packets from sender has a probability of being corrupted.

--- lab5_rdt3.0_sender_client.py
+++ lab5_rdt3.0_sender_client.py
@@ -29,7 +29,6 @@ def extract_data (received_packet):
     i = string.strip (i, '\'')
     received_tuple[j] = i
     j = j + 1
-  check = ip_checksum(received_tuple[1])
   #print type(check)
   #print 'checksum is', check, str(received_tuple[2])
   #uni = '\x97\x96'
@@ -41,7 +40,30 @@ def extract_data (received_packet):
   data = received_tuple[1]
   check_sum = received_tuple[2]
   return seq_num, data, check_sum
-  
+
+#################################
+#checksum cmp
+#################################
+def checksum_cmp (received_content, received_checksum):
+  #casting checksum to str by itself is not the same when casting it to str via tuple...
+  #when checksum is in tuple and is cast into a string, they are the same
+  check = ip_checksum(received_content)
+  tuple_pkt = (0,0, check)
+  tuple_pkt_str = str(tuple_pkt)
+  splice_tuple_pkt = string.split(tuple_pkt_str, ',')
+  j = 0
+  for i in splice_tuple_pkt:
+    i = string.strip(i, '(')
+    i = string.strip(i, ')')
+    i = string.strip (i) #by default it removes whitespace
+    i = string.strip (i, '\'')
+    splice_tuple_pkt[j] = i
+    j = j + 1
+  if splice_tuple_pkt[2] == received_checksum:
+    return 1
+  else:
+    return 0
+
 # create dgram udp socket
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -59,6 +81,7 @@ states = ['wait_for_call_0', 'wait_for_ACK0)', 'wait_for_call_1', 'wait_for_ACK1
 states_list = list(enumerate(states))
 current_state = states_list[0]
 #print current_state
+packet1_corrupt = 1
 
 while(1) :
   #wait for call 0, initial state
@@ -90,7 +113,6 @@ while(1) :
     outputs = []
     timeout = 5
     readable, writeable, expectional = select.select(inputs, outputs, inputs, timeout)
-    #check for corruption with checksum
     for temp_socket in readable:
       received_data = temp_socket.recvfrom(1024)
       received_packet = received_data[0]
@@ -99,7 +121,7 @@ while(1) :
       packet_sequence_number, packet_data, packet_checksum = extract_data(received_packet)
       #print packet_sequence_number, packet_data, packet_checksum
       #check if packet is ACK0, if so move to next state
-
+      #we would check for corrupt ACK as well, but for purpose of lab, we are omitting it
       if packet_data == 'ACK0' and packet_sequence_number == '0':
         current_state = states_list[2]
       #if checksum is valid move to next state
@@ -114,14 +136,20 @@ while(1) :
     if msg == 'exit':
         break;
     checksum = ip_checksum(msg)
-    #print 'Checksum is', str(checksum)
-    #print 'checksum is', checksum
-    #create packet
-    packet_to_send = make_packet('1', msg, checksum)
-    
-    #make a copy of packet for retransmission
-    copy_of_sent_packet = packet_to_send
-    print 'copy of packet is', copy_of_sent_packet
+    if packet1_corrupt:
+        packet1_corrupt = 0
+        #make a copy of packet for retransmission
+        copy_of_sent_packet = make_packet('1', msg, checksum)
+        checksum = checksum + '3' #modify the checksum in any manner to reflect corruption
+        #create packet
+        packet_to_send = make_packet('1', msg, checksum)
+        print 'copy of packet is', packet_to_send
+    elif not(packet1_corrupt):
+        #create packet
+        packet_to_send = make_packet('1', msg, checksum)
+        #make a copy of packet for retransmission
+        copy_of_sent_packet = packet_to_send
+        print 'copy of packet is', copy_of_sent_packet
     #send the packet
     s.sendto(str(packet_to_send),(host, port))
     #change to next state
